148f25053a0683df04675505b32be4c6f412c2de
Samuel-web363 <chinonsosamuel2654@gmail.com>
2025-06-17 07:14:52 -1100

    Added a few code

---
+++ index.js
@@ -0,0 +1,40 @@
+
+
+const userFirstAndLastName = "Chinonso Samuel"
+const myFavooriteBookTitlename = "Return Of Mount Hua Sect"
+const currentPageNumberForBook = "Page Eight"
+const thisISSamplTextMessage = "Daniel"
+const doesDanielLikeWomenALLot = "Yes"
+
+console.log(userFirstAndLastName.length)
+console.log(userFirstAndLastName.toLowerCase())
+console.log(userFirstAndLastName.includes("M"))
+console.log(userFirstAndLastName.endsWith("S"))
+console.log(userFirstAndLastName.charAt("3"))
+
+const theTotalNumberOfStudents = "8"
+const myCurrentBankAccountBalance = "0"
+const myFinalExamScoreResult = "4.5"
+const currentPageNumberForManga = "220"
+const thisRandomLargeNumberValue = "1000000"
+
+console.log(theTotalNumberOfStudents.length)
+console.log(theTotalNumberOfStudents.toLowerCase())
+console.log(theTotalNumberOfStudents.includes("M"))
+console.log(theTotalNumberOfStudents.endsWith("S"))
+console.log(theTotalNumberOfStudents.charAt("0"))
+
+const isThisTheTotalNumberHere = "True"
+const hasTheSystemBeenUpdatedYet = "True"
+const doYouHaveAnyMoney = "False"
+const thatMeansYouAreBroke = "True"
+const doesThisFeatureWorkProperly = "True"
+
+console.log(isThisTheTotalNumberHere.length)
+console.log(isThisTheTotalNumberHere.toLowerCase())
+console.log(isThisTheTotalNumberHere.includes("M"))
+console.log(isThisTheTotalNumberHere.endsWith("S"))
+console.log(isThisTheTotalNumberHere.charAt("3"))
+
+
+
